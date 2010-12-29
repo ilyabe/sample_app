0798dfb77e8b063fa1a8f7a98561b0b9d9d78737
Ilya <ilyabe@gmail.com>
2010-12-29 08:30:08 -0600

    Added password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,7 @@ class User < ActiveRecord::Base
 															:uniqueness	=> { :case_sensitive => false }
 	
 	# Automatically create the virtual attribute 'password_confirmation'
+	validates :password, :presence 			=> true,
+																	:confirmation 	=> true,
+																	:length 					=> { :within => 6..40 }
 end
