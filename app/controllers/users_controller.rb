06174637e5e743e2bae3b0b7913cd2d2ef9a0a72
Ilya <ilyabe@gmail.com>
2011-02-02 19:28:12 -0600

    Reset password if signup form submitted with errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,8 @@ class UsersController < ApplicationController
 		  redirect_to @user
 		else
 		  @title = "Sign up"
-		  render 'new'
+      @user.password = ""
+      render 'new'
 		end
 	 end
 end
