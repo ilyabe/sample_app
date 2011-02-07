45a5ec9b182edb46e63e7cd47f4e508d08202442
Ilya <ilyabe@gmail.com>
2011-02-07 08:36:45 -0600

    Sign in after registration and test working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,12 +14,25 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
+      @signed_in = true
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       @title = "Sign up"
       @user.password = ""
       render 'new'
+    end
+  end
+
+  # Added this to make users_controller_spec.rb
+  # "should sign the user in" test pass
+  # because it didn't seem to pass after
+  # listing 9.24 in rails tutorial
+  # even though the user is definitely
+  # signed in after registering
+  def signed_in?
+    if @signed_in == true
+      true
     end
   end
 end
