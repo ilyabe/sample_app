7f4e16aa2aa1f93feed576e3d824913fa5b7f9e9
Ilya <ilyabe@gmail.com>
2011-02-09 20:14:51 -0600

    Deleted secret

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SampleApp::Application.initialize!
-
-# For sessions
-config.action_dispatch.session = {
-  :key    => '_app_session',
-  :secret => '0x0dkfj3927dkc7djdh36rkckdfzsg'
-}
