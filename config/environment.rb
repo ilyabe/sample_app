e91763e5d737e4fe3b4057a1153120d4cc32d087
Ilya <ilyabe@gmail.com>
2011-02-09 20:08:41 -0600

    Added secret

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SampleApp::Application.initialize!
+
+# For sessions
+config.action_dispatch.session = {
+  :key    => '_app_session',
+  :secret => '0x0dkfj3927dkc7djdh36rkckdfzsg'
+}
